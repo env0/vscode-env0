19cb1826f1e7ac152702fe7edc3f11d991056898
Liran Farage <liran.farage@env0.com>
2022-09-11 15:58:57 +0300

    debug log

--- src/extension.ts
+++ src/extension.ts
@@ -4,11 +4,6 @@ export function activate(context: vscode.ExtensionContext) {
   vscode.window.createTreeView("env0-environments", {
     treeDataProvider: new Env0EnvironmentsProvider("some-project-id"),
   });
-
-  const configurationWorkspace = vscode.workspace.getConfiguration()
-  const env0ApiKey: string | undefined = configurationWorkspace.get("env0.apiKeyId")
-  console.log('apiKey is', env0ApiKey);
-  
 }
 
 export function deactivate() {}
