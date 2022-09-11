756ceaa1d0baf0b7444de8734beb1b040a19edf3
Ronny Orot <ronny.orot@env0.com>
2022-09-11 16:48:15 +0300

    revert changes

--- src/extension.ts
+++ src/extension.ts
@@ -1,12 +1,9 @@
 import * as vscode from "vscode";
 import { Env0EnvironmentsProvider } from "./env0-environments-provider";
-import { getEnvironmentsForBranch } from "./get-environments";
 export async function activate(context: vscode.ExtensionContext) {
   vscode.window.createTreeView("env0-environments", {
     treeDataProvider: new Env0EnvironmentsProvider("some-project-id"),
   });
-
-  await getEnvironmentsForBranch();
 }
 
 export function deactivate() {}
