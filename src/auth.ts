c7557512898b55fd517b6417edbc9e1d73219167
Ronny Orot <ronny.orot@env0.com>
2022-09-11 18:02:17 +0300

    use credentials from extensions settings

--- src/auth.ts
+++ src/auth.ts
@@ -9,6 +9,6 @@ export const getApiKeyCredentials = () => {
         throw new Error("Could not read env0 api key values");
     }
 
-    return { user: env0ApiKey, password: env0SecretKey }
+    return { username: env0ApiKey, password: env0SecretKey }
 
 }
