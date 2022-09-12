57d85fd827c07c7572606ca7db87f810c2d13ec5
Alon Kazimirsky <17432424+Wassap124@users.noreply.github.com>
2022-09-12 15:38:10 +0300

    reorganize

--- src/extension.ts
+++ src/extension.ts
@@ -1,5 +1,13 @@
 import axios from "axios";
 import * as vscode from "vscode";
+import {
+  abortEnvironmentDeploy,
+  cancelDeployment,
+  destroyEnvironment,
+  openEnvironmentInBrowser,
+  redeployEnvironment,
+  resumeDeployment,
+} from "./actions";
 import { getApiKeyCredentials } from "./auth";
 import { Env0EnvironmentsProvider } from "./env0-environments-provider";
 import { getEnvironmentsForBranch } from "./get-environments";
@@ -19,8 +27,10 @@ export function activate(context: vscode.ExtensionContext) {
   tree.onDidChangeSelection(async (e) => {
     const env = e.selection[0] ?? e.selection;
 
-    Object.values(logChannels).forEach((l: any) => (l.channel as vscode.OutputChannel).dispose());
-    Object.keys(logChannels).forEach(key => delete logChannels[key]);
+    Object.values(logChannels).forEach((l: any) =>
+      (l.channel as vscode.OutputChannel).dispose()
+    );
+    Object.keys(logChannels).forEach((key) => delete logChannels[key]);
     clearInterval(logPoller);
     if (env.id) {
       logPoller = await pollForEnvironmentLogs(env, logChannels);
@@ -52,7 +62,7 @@ export function activate(context: vscode.ExtensionContext) {
   });
 
   vscode.commands.registerCommand("env0.cancel", (env) => {
-    cancelEnvironment(env);
+    cancelDeployment(env);
     environmentsDataProvider.refresh();
   });
 
@@ -72,115 +82,52 @@ export function deactivate() {
   clearInterval(environmentPollingInstance);
 }
 
-const openEnvironmentInBrowser = ({ id, projectId }: any) => {
-  if (!id || !projectId) {
-    return;
-  }
-
-  vscode.env.openExternal(
-    vscode.Uri.parse(
-      `https://dev.dev.env0.com/p/${projectId}/environments/${id}`
-    )
-  );
-};
-
-const abortEnvironmentDeploy = (env: any) => {
-  const apiKeyCredentials = getApiKeyCredentials();
-  const id = env?.latestDeploymentLogId;
-
-  if (!id) {
-    return;
-  }
-
-  const redeployUrl = `https://${ENV0_BASE_URL}/environments/deployments/${id}/abort`;
-  axios.post(redeployUrl, {}, { auth: apiKeyCredentials });
-};
-
-
-const cancelEnvironment = (env: any) => {
-  const apiKeyCredentials = getApiKeyCredentials();
-  const id = env?.latestDeploymentLogId;
-
-  if (!id) {
-    return;
-  }
-
-  const redeployUrl = `https://${ENV0_BASE_URL}/environments/deployments/${id}/cancel`;
-  axios.put(redeployUrl, undefined, { auth: apiKeyCredentials });
-};
-
-const resumeDeployment = (env: any) => {
-  const apiKeyCredentials = getApiKeyCredentials();
-  const id = env?.latestDeploymentLogId;
-
-  if (!id) {
-    return;
-  }
-
-  const redeployUrl = `https://${ENV0_BASE_URL}/environments/deployments/${id}`;
-  axios.put(redeployUrl, undefined, { auth: apiKeyCredentials });
-};
-
-const redeployEnvironment = (env: any) => {
-  if (!env.id) {
-    return;
-  }
-
-  const apiKeyCredentials = getApiKeyCredentials();
-  const redeployUrl = `https://${ENV0_BASE_URL}/environments/${env.id}/deployments`;
-  axios.post(redeployUrl, {}, { auth: apiKeyCredentials });
-};
-
-const destroyEnvironment = (env: any) => {
-  if (!env.id) {
-    return;
-  }
-
-  const apiKeyCredentials = getApiKeyCredentials();
-  const redeployUrl = `https://${ENV0_BASE_URL}/environments/${env.id}/destroy`;
-  axios.post(redeployUrl, {}, { auth: apiKeyCredentials });
-};
-
 async function pollForEnvironmentLogs(env: any, logChannels: any) {
   const logPoller = setInterval(async () => {
     const apiKeyCredentials = getApiKeyCredentials();
 
     const options = {
-      method: 'GET',
+      method: "GET",
       url: `https://${ENV0_BASE_URL}/deployments/${env?.latestDeploymentLogId}/steps`,
-      auth: apiKeyCredentials
+      auth: apiKeyCredentials,
     };
 
     const response = await axios.request(options);
 
     (response.data as any).forEach(async (step: any) => {
       let stepLog = logChannels[step.name];
-      if(!stepLog) {
-        logChannels[step.name] = { channel: vscode.window.createOutputChannel(`(env0) ${step.name}`) };
+      if (!stepLog) {
+        logChannels[step.name] = {
+          channel: vscode.window.createOutputChannel(`(env0) ${step.name}`),
+        };
         stepLog = logChannels[step.name];
       }
 
       if (stepLog.hasMoreLogs !== false) {
         try {
           const response: any = await axios.get(
-            `https://${ENV0_BASE_URL}/deployments/${env?.latestDeploymentLogId}/steps/${step.name}/log?startTime=${stepLog.startTime ?? ''}`,
+            `https://${ENV0_BASE_URL}/deployments/${
+              env?.latestDeploymentLogId
+            }/steps/${step.name}/log?startTime=${stepLog.startTime ?? ""}`,
             {
-              auth: apiKeyCredentials
+              auth: apiKeyCredentials,
             }
           );
 
-          console.log('got response', {response});
+          console.log("got response", { response });
           response.data.events.forEach((event: any) => {
-            (logChannels[step.name].channel as vscode.OutputChannel).appendLine(event.message);
+            (logChannels[step.name].channel as vscode.OutputChannel).appendLine(
+              event.message
+            );
           });
           stepLog.startTime = response.data.nextStartTime;
           stepLog.hasMoreLogs = response.data.hasMoreLogs;
-          if (step.status === 'IN_PROGRESS') {
+          if (step.status === "IN_PROGRESS") {
             stepLog.channel.show();
-          }     
-        } catch(e) {
-          console.error('oh no', {e});
-        }   
+          }
+        } catch (e) {
+          console.error("oh no", { e });
+        }
       }
     });
   }, 3000);
