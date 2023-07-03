230ace6d7ff50da4a1065557849fbfc12da5092c
meni <meni.roytenburd@gmail.com>
2023-07-03 20:56:20 +0300

    minor fix in jest config

--- jest.config.js
+++ jest.config.js
@@ -169,7 +169,7 @@ module.exports = {
   // transform: undefined,
 
   // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
-  transformIgnorePatterns: ["/node_modules/(?!(strip-ansi)/)"],
+  transformIgnorePatterns: ["/node_modules/"],
 
   // An array of regexp pattern strings that are matched against all modules before the module loader will automatically return a mock for them
   // unmockedModulePathPatterns: undefined,
