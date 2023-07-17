7ed084e7c92b88a63af51197d0363d8165959e61
meni <meni.roytenburd@gmail.com>
2023-07-17 15:01:08 +0300

    added roots and moduleDirectories same error

--- jest.it.config.js
+++ jest.it.config.js
@@ -1,11 +1,13 @@
 const path = require("path");
 
 module.exports = {
+  roots: ["<rootDir>"],
   moduleFileExtensions: ["js"],
   verbose: true,
   testMatch: ["<rootDir>/out/test/integration/suite/**.test.it.js"],
   modulePathIgnorePatterns: ["node_modules/"],
   testEnvironment: "<rootDir>/out/test/integration/vscode-env.js",
+  moduleDirectories: ["src", "node_modules"],
   moduleNameMapper: {
     vscode: "<rootDir>/src/test/integration/vscode.js",
   },
--- src/test/integration/suite/init.test.it.ts
+++ src/test/integration/suite/init.test.it.ts
@@ -2,6 +2,7 @@ import vscode from "vscode";
 
 describe("Extension Test Suite", () => {
   it("Sample test", async () => {
+    expect(Object.keys(vscode).length).toBeGreaterThan(1);
     await vscode.window.showInformationMessage("Hello World from yotest!");
     expect(1).toBe(1);
   });
