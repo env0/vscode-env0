d5dfb19dde1b0ee3e3dbb1eaf934293d557c9b16
meni <meni.roytenburd@gmail.com>
2023-07-17 17:51:48 +0300

    used __dirname instead of process.cwd to get root error Cannot find module './babel.config.js'

--- src/test/integration/suite/index.ts
+++ src/test/integration/suite/index.ts
@@ -2,7 +2,8 @@ import * as path from "path";
 
 import * as jest from "jest";
 export async function run() {
-  const projectRootPath = process.cwd();
+  const projectRootPath = path.resolve(__dirname, "../../../../");
+
   const config = path.resolve(projectRootPath, "jest.it.config.js");
 
   await jest.runCLI(
