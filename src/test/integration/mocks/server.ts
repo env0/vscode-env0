f966079b900c433c357436618a000f152eda60d2
meni <meni.roytenburd@gmail.com>
2023-07-25 21:16:41 +0300

    should not show inactive environments (failing)

--- src/test/integration/mocks/server.ts
+++ src/test/integration/mocks/server.ts
@@ -4,6 +4,7 @@ import { setupServer } from "msw/node";
 import { before, after, afterEach } from "mocha";
 import { ENV0_API_URL } from "../../../common";
 import { EnvironmentModel } from "../../../get-environments";
+import { Credentials } from "../../../types";
 
 const server = setupServer(
   rest.all(`*`, (_req, res, ctx) => {
@@ -11,30 +12,24 @@ const server = setupServer(
   })
 );
 
-const assertAuth = (
-  auth: { keyId: string; secret: string },
-  authHeader: string | null
-) => {
+const assertAuth = (credentials: Credentials, authHeader: string | null) => {
   if (!authHeader) {
     throw new Error("no auth header");
   }
   const authBase64 = authHeader.split("Basic ")[1];
   const [keyId, secret] = atob(authBase64 || "").split(":");
-  assert.strictEqual(keyId, auth.keyId);
-  assert.strictEqual(secret, auth.secret);
+  assert.strictEqual(keyId, credentials.keyId);
+  assert.strictEqual(secret, credentials.secret);
 };
 
 export const mockGetOrganization = (
   organizationId: string,
-  auth?: {
-    keyId: string;
-    secret: string;
-  }
+  credentials?: Credentials
 ) => {
   server.use(
     rest.get(`https://${ENV0_API_URL}/organizations`, (req, res, ctx) => {
-      if (auth) {
-        assertAuth(auth, req.headers.get("Authorization"));
+      if (credentials) {
+        assertAuth(credentials, req.headers.get("Authorization"));
       }
       return res(ctx.json([{ id: organizationId }]));
     })
@@ -42,10 +37,14 @@ export const mockGetOrganization = (
 };
 export const mockGetEnvironment = (
   organizationId: string,
-  environments: EnvironmentModel[]
+  environments: EnvironmentModel[],
+  credentials?: Credentials
 ) => {
   server.use(
     rest.get(`https://${ENV0_API_URL}/environments`, (req, res, ctx) => {
+      if (credentials) {
+        assertAuth(credentials, req.headers.get("Authorization"));
+      }
       if (
         new URL(req.url as any).searchParams.get("organizationId") ===
         organizationId
--- src/test/integration/suite/init.test.it.ts
+++ src/test/integration/suite/init.test.it.ts
@@ -1,53 +1,72 @@
 import * as assert from "assert";
-import * as vscode from "vscode";
-import sinon from "sinon";
 import { mockGetEnvironment, mockGetOrganization } from "../mocks/server";
 import { mockGitRepoAndBranch } from "../mocks/git";
-import retry from "async-retry";
-
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
 import * as extension from "../../../../dist/extension.js";
 import { Env0EnvironmentsProvider } from "../../../env0-environments-provider";
-
-const auth = { keyId: "key-id", secret: "key-secret" };
+import { getEnvironmentMock, login, logout, waitFor } from "./test-utils";
+import { afterEach } from "mocha";
+import sinon from "sinon";
 
 suite("init", () => {
+  afterEach(async () => {
+    sinon.restore();
+    await logout();
+  });
+
   test("should show environments", async () => {
+    const auth = { keyId: "key-id", secret: "key-secret" };
+
     const orgId = "org-id";
     const envName = "my env";
-    mockGetOrganization(orgId, auth);
-    const inputStub = sinon.stub(vscode.window, "showInputBox");
-    inputStub.onFirstCall().resolves(auth.keyId);
-    inputStub.onSecondCall().resolves(auth.secret);
-    mockGetEnvironment(orgId, [
-      {
-        id: "env-1",
+    const environments = [
+      getEnvironmentMock("main", "https://github.com/user/repo", {
         name: envName,
-        projectId: "project-id",
-        status: "ACTIVE",
-        latestDeploymentLog: {
-          id: "id",
-          blueprintRevision: "main",
-          blueprintRepository: "https://github.com/user/repo",
-        },
-      } as any,
-    ]);
+      }),
+    ];
+    mockGetOrganization(orgId, auth);
+    mockGetEnvironment(orgId, environments, auth);
     mockGitRepoAndBranch("main", "git@github.com:user/repo.git");
-    vscode.commands.executeCommand("env0.login");
+
+    await login(auth);
 
     const environmentsDataProvider =
       extension.environmentsDataProvider as Env0EnvironmentsProvider;
-    await retry(
-      () => {
-        const environmentItem = environmentsDataProvider.getChildren()[0];
-        assert.strictEqual(environmentItem.label, envName);
-      },
-      {
-        factor: 1,
-        minTimeout: 1000,
-        retries: 10,
-      }
-    );
-  }).timeout(1000 * 60);
+
+    await waitFor(() => {
+      const environments = environmentsDataProvider.getChildren();
+      assert.strictEqual(environments.length, 1);
+      assert.strictEqual(environments[0].label, envName);
+    });
+  });
+
+  test("should not show inactive environments", async () => {
+    const auth = { keyId: "key-id2", secret: "key-secret2" };
+
+    const orgId = "org-id";
+    const envName = "my env 2";
+    const environments = [
+      getEnvironmentMock("main", "https://github.com/user/repo", {
+        name: envName,
+      }),
+      getEnvironmentMock("main", "https://github.com/user/repo", {
+        status: "INACTIVE",
+      }),
+    ];
+    mockGetOrganization(orgId, auth);
+    mockGetEnvironment(orgId, environments, auth);
+    mockGitRepoAndBranch("main", "git@github.com:user/repo.git");
+
+    await login(auth);
+
+    const environmentsDataProvider =
+      extension.environmentsDataProvider as Env0EnvironmentsProvider;
+
+    await waitFor(() => {
+      const environments = environmentsDataProvider.getChildren();
+      assert.strictEqual(environments.length, 1);
+      assert.strictEqual(environments[0].label, envName);
+    });
+  });
 });
