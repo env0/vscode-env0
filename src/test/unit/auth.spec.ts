391077fc575c97b5368804fde2350da8f7ece97a
meni <meni.roytenburd@gmail.com>
2023-08-30 16:24:45 +0300

    vscode mock refactor, assert secrets store

--- src/test/unit/auth.spec.ts
+++ src/test/unit/auth.spec.ts
@@ -1,40 +1,38 @@
 import * as jestMock from "jest-mock";
 import expect from "expect";
+import axios from "axios";
 // vscode mock must be imported before the tested module!!!
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const { vscode } = require("./mocks/vscode");
+import {
+  contextMock,
+  getRegisteredCommand,
+  mockLogin,
+  resetVscodeMocks,
+} from "./mocks/vscode";
 // eslint-disable-next-line import/first
 import { AuthService } from "../../auth";
 
 describe("authentication", () => {
+  afterEach(() => {
+    resetVscodeMocks();
+  });
+
   it("should save login credentials in secret store", async () => {
-    let registerCommand: string;
-    let registeredLoginCommand: () => any;
     const keyId = "keyId";
     const secret = "secret";
-    vscode.commands = {
-      registerCommand: (command: string, onCommand: () => any) => {
-        registerCommand = command;
-        registeredLoginCommand = onCommand;
-      },
-    };
-    vscode.window = {
-      showInformationMessage: jestMock.fn(),
-      showInputBox: jestMock
-        .fn<any>()
-        .mockResolvedValueOnce(keyId)
-        .mockResolvedValueOnce(secret),
-      withProgress: jestMock.fn(),
-    };
-    const context = {
-      subscriptions: [],
-      secrets: { get: () => {} },
-    };
-    const auth = new AuthService(context as any);
+    mockLogin(keyId, secret);
+    // todo extract this
+    axios.get = jestMock.fn<any>().mockResolvedValueOnce({ status: 200 });
+    const auth = new AuthService(contextMock);
     const onLogin = jestMock.fn();
     auth.registerLoginCommand(onLogin);
-    expect(registerCommand!).toBe("env0.login");
-    await registeredLoginCommand!();
+    const loginCommand = getRegisteredCommand("env0.login");
+    await loginCommand();
     expect(onLogin).toHaveBeenCalled();
+    expect(contextMock.secrets.store).toHaveBeenCalledWith("env0.keyId", keyId);
+    expect(contextMock.secrets.store).toHaveBeenCalledWith(
+      "env0.secret",
+      secret
+    );
   });
 });
--- src/test/unit/mocks/vscode.ts
+++ src/test/unit/mocks/vscode.ts
@@ -1,3 +1,37 @@
 import mock from "mock-require";
-export const vscode = { commands: {}, window: {} };
+import { ModuleMocker } from "jest-mock";
+const mocker = new ModuleMocker(global);
+const registeredCommands: Record<string, () => any> = {};
+export const vscode = {
+  commands: {
+    registerCommand: (command: string, onCommand: () => any) => {
+      registeredCommands[command] = onCommand;
+    },
+  },
+  window: {
+    showInformationMessage: mocker.fn(),
+    showInputBox: mocker.fn(),
+    withProgress: (options: any, task: any) => task(),
+  },
+};
 mock("vscode", vscode);
+
+export const mockLogin = (keyId: string, secret: string) => {
+  vscode.window.showInputBox = mocker
+    .fn<any>()
+    .mockResolvedValueOnce(keyId)
+    .mockResolvedValueOnce(secret);
+};
+
+export const contextMock = {
+  subscriptions: [],
+  secrets: { get: mocker.fn(), store: mocker.fn(), delete: mocker.fn() },
+} as any;
+
+export const resetVscodeMocks = () => {
+  mocker.resetAllMocks();
+};
+
+export const getRegisteredCommand = (command: string) => {
+  return registeredCommands[command];
+};
